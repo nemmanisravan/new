3c1bbcec40840d2e2d6fe50701a2cf03d1a342bd
nemmanisravan <61456508+nemmanisravan@users.noreply.github.com>
2020-10-16 16:37:48 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,7 +1,7 @@
  provider "azurerm" {  
           #version = "=2.20.0"  
          # version = "=2.0.16" 
-           version = "=2.20.0"
+          version = "=0.12.3"
            subscription_id = "${var.subscription_id}" 
             tenant_id       = "${var.tenant_id}"  
             client_id       = "${var.client-id}"  
